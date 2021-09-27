52568a0fa78e2b19013c2a6dad907696c1a8520c
Tieguyenn <tieguyenn21@gmail.com>
2021-09-27 09:10:06 +0700

    update bt 3

--- resources/views/bt/bt3.blade.php
+++ resources/views/bt/bt3.blade.php
@@ -2,6 +2,31 @@
 
 @section('title', 'Bai Tap 3')
 @section('content')
-	<div>
-	</div>
+<div>
+	<?php
+	$arr = [];
+	for($i=1;$i<=100;$i++) {
+		$arr[] = $i;
+	}
+	echo implode('-', $arr).'<br>';
+	$arr = [];
+	for($i=1;$i<=200;$i++) {
+		$arr[] = $i;
+	}
+	echo implode('-', $arr).'<br>';
+	echo 'Summary from 1 to 200: '.array_sum($arr).'<br>';
+	$number = 10;
+	$factorial = 1;
+	for ($i = 1; $i <= $number; $i++){
+		$factorial = $factorial * $i;
+	}
+	echo "input: $number <br>";
+	echo "output: $factorial <br>";
+	$string = 'Hello Everyone';
+	echo "$string <br>";
+	$pos = rand(0,strlen($string));
+	$char = substr($string, $pos,1);
+	echo "Random character: $char - $pos<br>";
+	?>
+</div>
 @endsection
