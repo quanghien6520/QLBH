8119c6b3273e9428175665a4a52d3444efdc2424
quanghien6520 <quanghien6520@gmail.com>
2022-01-20 19:27:26 +0700

    update layout

--- resources/views/layout/mainLayout.blade.php
+++ resources/views/layout/mainLayout.blade.php
@@ -7,6 +7,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>@yield('title')</title>
    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+   <script src="{{asset('js/jquery.min.js')}}"></script>
 </style>
 </head>
 <!-- END: Head-->
@@ -16,7 +17,7 @@
 <!-- BEGIN: Content-->
      @yield('content')
 <!-- END: Content-->
-
+@yield('scripts')
 </body>
 <!-- END: Body-->
 </html>
